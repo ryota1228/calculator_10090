ad478098e641cb93afd80b6f329a196cc76d1829
ryota1228 <ryota.yamazaki@pathoslogos.co.jp>
2025-04-07 15:18:34 +0900

    計算機1stコミット

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'calculator10090_2' title`, () => {
+  it(`should have the 'calculator-app2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('calculator10090_2');
+    expect(app.title).toEqual('calculator-app2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculator10090_2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, calculator-app2');
   });
 });
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() label!: string;
+  @Output() press = new EventEmitter<string>();
+
+  onClick() {
+    this.press.emit(this.label);
+  }
+}
